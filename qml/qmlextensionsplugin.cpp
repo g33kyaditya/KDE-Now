256cf8a51a3370a2c2fb2bc7b5245f18c74bd924
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-04-10 12:28:41 +0530

    Add qmlRegisterType

--- qml/qmlextensionsplugin.cpp
+++ qml/qmlextensionsplugin.cpp
@@ -13,5 +13,8 @@ void QmlExtensionsPlugin::initializeEngine(QQmlEngine *engine, const char *uri)
 void QmlExtensionsPlugin::registerTypes(const char *uri)
 {
     qmlRegisterType<FlightReservation>(uri, 0, 1, "FlightReservation")
+    qmlRegisterType<HotelReservation>(uri, 0, 1, "HotelReservation")
+    qmlRegisterType<EventReservation>(uri, 0, 1, "EventReservation")
+    qmlRegisterType<RestaurantReservation>(uri, 0, 1, "RestaurantReservation")
 }
 
